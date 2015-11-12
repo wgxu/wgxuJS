4459eb12937741ed2a3e302a40900bb457d81140
wgxu <873413442@.com>
2015-11-12 14:44:11 +0800

    nodejs 聊天室demo

---
+++ demo3/server.js
@@ -0,0 +1,26 @@
+var http = require('http'),
+    io = require('socket.io');
+
+var conns = [];
+
+
+var server = http.createServer().listen(8888,'0.0.0.0',function(){
+    console.log('server starting... the port is 8888');
+});
+
+io = io.listen(server);
+
+io.sockets.on('connection',function(socket){
+    conns.push(socket);
+    console.log('conned');
+    socket.emit('news', { hello: '欢迎加入聊天室' });
+
+
+    //收到消息
+    socket.on('message',function(data){
+       for(var i=0;i<conns.length;i++){
+         conns[i].emit("message",data);
+       } 
+    });
+});
+
